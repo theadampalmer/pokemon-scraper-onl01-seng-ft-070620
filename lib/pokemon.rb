304a6414e79e236dc3393170b7d7458b676babf3
Adam Palmer <theadampalmer@gmail.com>
2020-08-12 23:53:32 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,31 @@
 class Pokemon
+attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type 
+        @db = db 
+    end 
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?)
+        SQL
+        db.execute(sql, name, type)
+    end 
+
+    def self.find(id, db)
+        sql = <<-SQL
+        SELECT * 
+        FROM pokemon
+        WHERE id = ?
+        LIMIT 1
+        SQL
+        db.execute(sql, id).collect do |row|
+        self.new(id: row[0], name: row[1], type: row[2], db: db)
+        end.first
+    end 
+
 end
